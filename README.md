9cd036da3fa49c6f95f9beb95cd35b7dc53bcb06
faris <mrfaristp@gmail.com>
2023-09-17 16:27:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bizzbuddy
+This is a social media application using react as frontend and backend using node 
+
+## under construction
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='font-bold underline text-4xl'>hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
