b341c1651692342c9a18ece4033b0bf1e11d04d7
faris <mrfaristp@gmail.com>
2023-10-03 14:25:12 +0530

    dotenv modifed

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -6,6 +5,7 @@ import dbConnect from "./config/config.js";
 import userRouter from "./routers/userRoute.js";
 import adminRouter from './routers/adminRoute.js'
 
+// import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
