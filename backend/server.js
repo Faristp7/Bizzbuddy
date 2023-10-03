3fd0a99f14de21ec4552c4b51cefdc22ba863722
faris <mrfaristp@gmail.com>
2023-10-03 14:34:27 +0530

    npm i added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
@@ -5,7 +6,6 @@ import dbConnect from "./config/config.js";
 import userRouter from "./routers/userRoute.js";
 import adminRouter from './routers/adminRoute.js'
 
-// import "dotenv/config";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
